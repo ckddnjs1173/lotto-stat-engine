f695ba477f96b707d9965e8a6f4a356a9d6bde2b
ckddnjs1173 <ckddnjs0929@gmail.com>
2026-08-19 16:45:10 +0900

    Test frozen stable entrypoints

--- tests/test_current_entrypoints.py
+++ tests/test_current_entrypoints.py
@@ -1,13 +1,13 @@
 import unittest
 
 import main as root_main
-from scripts import run_recommend, run_v31_final_recommend
+from scripts import run_recommend, run_v31_frozen_recommend
 
 
 class CurrentEntrypointTests(unittest.TestCase):
-    def test_root_and_stable_runner_use_current_v31_main(self):
-        self.assertIs(root_main.main, run_v31_final_recommend.main)
-        self.assertIs(run_recommend.main, run_v31_final_recommend.main)
+    def test_root_and_stable_runner_use_frozen_v31_main(self):
+        self.assertIs(root_main.main, run_v31_frozen_recommend.main)
+        self.assertIs(run_recommend.main, run_v31_frozen_recommend.main)
 
 
 if __name__ == "__main__":
